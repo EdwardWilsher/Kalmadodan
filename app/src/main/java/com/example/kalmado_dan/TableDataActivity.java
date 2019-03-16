b64898d6f3f7857f1b86054333ca45be6872e3f9
DanielW-A <Danwa@users.noreply.github.com>
2019-03-16 14:54:11 +0000

    dynamically created the table

--- app/src/main/java/com/example/kalmado_dan/TableDataActivity.java
+++ app/src/main/java/com/example/kalmado_dan/TableDataActivity.java
@@ -1,7 +1,12 @@
 package com.example.kalmado_dan;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class TableDataActivity extends AppCompatActivity {
 
@@ -9,5 +14,42 @@ public class TableDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_table_data);
+
+        TableLayout stk = (TableLayout) findViewById(R.id.table_main);
+        TableRow tbrow0 = new TableRow(this);
+        TextView tv0 = new TextView(this);
+        tv0.setText(" Anxiety  \n Severity ");
+        tbrow0.addView(tv0);
+        TextView tv1 = new TextView(this);
+        tv1.setText(" Date ");
+        tbrow0.addView(tv1);
+        TextView tv2 = new TextView(this);
+        tv2.setText(" Description ");
+        tbrow0.addView(tv2);
+        TextView tv3 = new TextView(this);
+        tv3.setText("  ");
+        tbrow0.addView(tv3);
+        stk.addView(tbrow0);
+        for (int i = 0; i < 25; i++) {
+            TableRow tbrow = new TableRow(this);
+            TextView t1v = new TextView(this);
+            t1v.setText("" + i);
+            t1v.setGravity(Gravity.CENTER);
+            tbrow.addView(t1v);
+            TextView t2v = new TextView(this);
+            t2v.setText(i + "/12/12");
+            t2v.setGravity(Gravity.CENTER);
+            tbrow.addView(t2v);
+            TextView t3v = new TextView(this);
+            t3v.setText("hard 42 character limit------------------|");
+            t3v.setGravity(Gravity.CENTER);
+            tbrow.addView(t3v);
+            TextView t4v = new TextView(this);
+            t4v.setText("Delete");
+            t4v.setGravity(Gravity.CENTER);
+            tbrow.addView(t4v);
+            stk.addView(tbrow);
+        }
+
     }
 }
