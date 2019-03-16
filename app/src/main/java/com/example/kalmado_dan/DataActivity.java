4151b140564239c5b0ee1cf73ba5ca675d0411e0
DanielW-A <Danwa@users.noreply.github.com>
2019-03-16 16:16:09 +0000

    Added basic graph

--- app/src/main/java/com/example/kalmado_dan/DataActivity.java
+++ app/src/main/java/com/example/kalmado_dan/DataActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class DataActivity extends AppCompatActivity {
 
     private Button Home;
@@ -32,6 +36,16 @@ public class DataActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
 
     }
 }
